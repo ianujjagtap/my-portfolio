4a9fc908063383a84c85f140870b5c8d064a2166
ianujjagtap <anujjagtap2004@gmail.com>
2025-07-26 23:03:17 +0530

    chore: projects section update

--- src/components/sections/projects.jsx
+++ src/components/sections/projects.jsx
@@ -1,48 +1,6 @@
+import { projects } from "../../providers/data";
 import { ProjectIcon } from "../svgs/project-icon";
 
-const projects = [
-	{
-		year: "2024 - 25 ",
-		name: "Echo",
-		madeAt: "Personal Project",
-		tech: [
-			"Vite",
-			"React",
-			"Tailwind CSS",
-			"Node.js",
-			"Express",
-			"MongoDB",
-			"Framer Motion",
-		],
-		links: [
-			{ name: "Live", url: "https://echo-the-chatbot.vercel.app" },
-			{ name: "GitHub", url: "https://github.com/ianujjagtap/Echo" },
-		],
-	},
-	{
-		year: 2024,
-		name: "Thirty Six Studio",
-		madeAt: "Personal Project",
-		tech: ["React", "Vite", "Tailwind CSS", "GSAP"],
-		links: [{ name: "Live", url: "https://thirty-six.vercel.app" }],
-	},
-	{
-		year: 2025,
-		name: "Route LLM",
-		madeAt: "Personal Project",
-		tech: [
-			"Next.js",
-			"TypeScript",
-			"Vercel AI SDK",
-			"Prisma",
-			"PostgreSQL",
-			"Hono.js",
-		],
-		links: [
-			{ name: "GitHub", url: "https://github.com/ianujjagtap/route-llm" },
-		],
-	},
-];
 export const Projects = () => {
 	return (
 		<section id="projects" className="mb-36 scroll-mt-16">
---
+++ src/providers/data.js
@@ -0,0 +1,41 @@
+export const projects = [
+	{
+		year: "2024 - 25 ",
+		name: "Echo",
+		madeAt: "Personal Project",
+		tech: [
+			"Vite",
+			"React",
+			"Tailwind CSS",
+			"Node.js",
+			"Express",
+			"MongoDB",
+			"Framer Motion",
+		],
+		links: [
+			{ name: "Live", url: "https://echo-the-chatbot.vercel.app" },
+			{ name: "GitHub", url: "https://github.com/ianujjagtap/Echo" },
+		],
+	},
+	{
+		year: 2025,
+		name: "Route LLM",
+		madeAt: "Personal Project",
+		tech: [
+			"Next.js",
+			"TypeScript",
+			"Vercel AI SDK",
+			"Prisma",
+			"PostgreSQL",
+			"Hono.js",
+		],
+		links: [{ name: "Live", url: "https://route-llm.vercel.app/" }],
+	},
+	{
+		year: 2024,
+		name: "Thirty Six Studio",
+		madeAt: "Personal Project",
+		tech: ["React", "Vite", "Tailwind CSS", "GSAP"],
+		links: [{ name: "Live", url: "https://thirty-six.vercel.app" }],
+	},
+];
